fb3a7ca1778d44b97774cd8a55adac29966d7b4a
blaws006 <blaws006@gmail.com>
2023-09-30 11:43:28 -0400

    Added wether to nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
 console.log('javascript connected!');
 
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = `Richmond`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        displayWeather(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchWeather();
+
+const displayWeather = (weather) => {
+    const weatherIcon = document.getElementById('weather-icon');
+    const weatherImg = document.createElement('img');
+    const iconId = weather.weather[0].icon;
+    weatherImg.src = `https://openweathermap.org/img/w/${iconId}.png`;
+    weatherIcon.appendChild(weatherImg);
+    document.getElementById('weather-temp').textContent = weather.main.temp + '°F';
+    document.getElementById('weather-description').textContent = weather.weather[0].description
+} 
+
 const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 2000,
     pause: false
-})
+});
 
 // when the pause button is clicked, pause the carousel
 const carouselButton = document.getElementById('carouselButton');
-const faIcon = document.getElementById('faButton')
+const faIcon = document.getElementById('faButton');
 
-carouselButton.addEventListener('click', function() {
+carouselButton.addEventListener('click', function () {
     if (faIcon.classList.contains('fa-pause')) {
         faIcon.classList.remove('fa-pause');
         faIcon.classList.add('fa-play');
@@ -20,15 +47,3 @@ carouselButton.addEventListener('click', function() {
         carousel.cycle();
     }
 })
-
-// carouselPause.addEventListener('click', function() {
-//     console.log('pausing the carousel');
-//     carousel.pause();
-// })
-
-// const carouselPlay = document.getElementById('carouselPlay');
-
-// carouselPlay.addEventListener('click', function() {
-//     console.log('cycle through the carousel');
-//     carousel.cycle();
-// })
